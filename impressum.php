104beedcc9557527ce7961359e9d31926706af29
Pfonks <rndpfonks@pm.me>
2021-05-21 12:30:35 +0200

    Updated some references

--- impressum.php
+++ impressum.php
@@ -3,7 +3,7 @@
 
     echo $header->getHeader($i18n->get('title'), $i18n->get('impressum'), array('impressum.css'));
     
-    echo '<div id="impressumField">PPI - Version: ' . Constants::VERSION . '<br><br><br>' . $i18n->get('impressumContent') . '</div>';
+    echo '<div id="impressumField">RRI - Version: ' . Constants::VERSION . '<br><br><br>' . $i18n->get('impressumContent') . '</div>';
 
     echo $footer->getFooter();
 ?>
